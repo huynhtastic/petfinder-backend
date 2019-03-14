b902a6f2bed5be2b536dd183b67adcebfe4ae4bd
Richard L Huynh <huynhtastic@gmail.com>
2019-03-14 15:29:26 -0500

    can now request a token from v2

--- src/env.js
+++ src/env.js
@@ -1,4 +1,6 @@
 if (!process.env.PET_KEY) { throw new Error('Need to define PET_KEY env variable'); }
 export default {
   petKey: process.env.PET_KEY,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 };
--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,67 @@
 import buildUrl from 'build-url';
+import fetch from 'node-fetch';
 import env from './env';
 
 const petUrl = 'http://api.petfinder.com';
+const petv2 = 'https://api.petfinder.com/v2';
 export const petMethods = {
     GET: 'pet.get',
     RANDOM: 'pet.getRandom',
     FIND: 'pet.find',
   }
 
+var token = {};
+
+
+async function __requestToken() {
+  // Build everything needed to request a new token
+  const path = 'oauth2/token';
+  const url = buildUrl(petv2, { path: path });
+  const body = {
+    grant_type: 'client_credentials',
+    client_id: env.clientId,
+    client_secret: env.clientSecret,
+  };
+
+  // Create a request for a new token
+  console.log(url);
+  try {
+    const tokenRes = await fetch(url, {
+      method: 'POST',
+      body:   JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    var tokenJson = await tokenRes.json();
+  } catch (err) {
+    console.log(err);
+  };
+
+  // Create a new expiration time for us to compare by adding response expire
+  // time to our current time
+  const currentTime = Math.floor((new Date).getTime() / 1000);
+  console.log('tokenJson', tokenJson);
+  tokenJson['expires_in'] += currentTime;
+  token = tokenJson;
+}
+
+export function getToken() {
+  // check if token is empty
+  if (Object.entries(token).length === 0 && token.constructor === Object) {
+    // token is empty; request a new one
+    __requestToken();
+  } else if (token['expires_in'] < Math.floor((new Date).getTime() / 1000)) {
+    // token expired; request a new one
+    __requestToken();
+  }
+  return token['access_token'];
+}
+
 export function buildPetUrl(params, petMethod) {
   // build query params by including apikey and requesting json
   params.key = env.petKey;
   params.format = 'json';
 
+  getToken()
   console.log('buildpeturl params', params);
   let url = buildUrl(petUrl, {
     path: petMethod,
