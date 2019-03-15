1c3d3fd1e4fcf2485847891ffaae2a41ff2b281c
Richard L Huynh <huynhtastic@gmail.com>
2019-03-14 23:02:49 -0500

    getTypes now populating hardcoded values for common search fields

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -30,10 +30,51 @@ export async function get(req, res, next) {
   }
 }
 
+/**
+ * Convert the JSON from the getTypes petV2 call from an array of animal
+ * objects to an object of animal objects.
+ *
+ * @param req {object} - Request made from a client for available animals.
+ * @param res {object} - Response to send back to the client
+ */
 export async function getTypes(req, res, next) {
   try {
-    const json = await fetchV2ForJson(endpoints.ALLTYPES)
-    res.status(200).json(json);
+    const json = await fetchV2ForJson(endpoints.ALLTYPES);
+    const validParams = {
+      name: [{
+        placeholder: 'Enter a name to search for.',
+        allowClear: true,
+      }, 'text'],
+      location: [{
+        placeholder: '[Zip code] or [City, State]',
+        allowClear: true,
+      }, 'text'],
+      type: [[], 'select'],
+      breed: [[], 'select'],
+      size: [['Small', 'Medium', 'Large', 'XL'], 'select'],
+      gender: [[], 'select'],
+      age: [['Baby', 'Young', 'Adult', 'Senior'], 'select'],
+      coat: [[], 'select'],
+      status: [['Adoptable', 'Adopted', 'Found'], 'select'],
+    };
+
+    let populatedTypes = {};
+    for (let animalObj of json.types) {
+      let convAnimalObj = {};
+      for (let [animalProp, animalValue] of Object.entries(animalObj)) {
+        if (animalObj.hasOwnProperty(animalProp) && animalProp !== 'name') {
+          // Name is plural; need to be singular
+          const singularProp = animalProp.substring(0, animalProp.length-1);
+          convAnimalObj[singularProp] = animalValue;
+        }
+      }
+      populatedTypes[animalObj['name']] = convAnimalObj;
+    }
+
+    res.status(200).json({
+      types: populatedTypes,
+      validParams: validParams
+    });
   } catch (err) {
     next(err);
   }
